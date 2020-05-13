df5be3000fa7c924c2a59f4c7c704a136d28bfee
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2020-05-13 20:26:38 +0200

    [ fix ] Scala resolution bug for imports in defs

--- tests/imports/import-from-imported-object-in-def.scala
+++ tests/imports/import-from-imported-object-in-def.scala
@@ -1,10 +1,9 @@
 object c {
   def g(): Unit = { 
     import a.{h => _, b};
-    // imports are sequenced, so the imported h is not in 
-    // scope here but the locally defined h is
     val x: Int = h();
-    import b.h;
+    import b.{h,i};
+    val y: Int = i();
   };
   
   def h(): Int = 42;
@@ -14,5 +13,6 @@ object a {
   def h(): Unit = {};
   object b {
     def h(): Unit = {};
+    def i(): Int  = 42;
   };
 };
