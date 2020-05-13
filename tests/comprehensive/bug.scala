d0a16f988873bfb7fa526b9a25bb5d277c96e9a1
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2020-05-13 12:40:02 +0200

    [ add ] new tests

--- tests/comprehensive/bug.scala
+++ tests/comprehensive/bug.scala
@@ -1,6 +1,6 @@
 object c {
-  import a._;
   def g(): Unit = { 
+    import a.{h => _, b};
     // imports are sequenced, so the imported h is not in 
     // scope here but the locally defined h is
     val x: Int = h();
